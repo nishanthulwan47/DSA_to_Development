b40ea50c94318ec927a72085af022c537b661a18
nishanthulwan <nishanthulwan47@gmail.com>
2024-03-20 13:12:23 -0400

    pushing latest Dev changes

---
+++ src/sumofNNaturalNumbers.java
@@ -0,0 +1,18 @@
+public class sumofNNaturalNumbers {
+
+
+    static int findSum(int n) {
+
+        return n * (n + 1) / 2;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(findSum(5));
+    }
+
+    //Time Complexity is O(1)
+
+    // Space Complexity is O(1)
+
+}
